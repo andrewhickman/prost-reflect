7cba3e7f45f08c782f3414733a9e56edeec27328
Andrew Hickman <andrew.hickman1@sky.com>
2021-12-25 01:42:20 +0000

    Add (failing) test for scalar arrays

--- tests/src/lib.rs
+++ tests/src/lib.rs
@@ -43,6 +43,30 @@ fn roundtrip_scalars() {
     );
 }
 
+#[test]
+fn roundtrip_scalar_arrays() {
+    roundtrip(
+        &ScalarArrays {
+            double: vec![1.1, 2.2],
+            float: vec![3.3f32, 4.4f32],
+            int32: vec![5, -6],
+            int64: vec![7, -8],
+            uint32: vec![9, 10],
+            uint64: vec![11, 12],
+            sint32: vec![13, -14],
+            sint64: vec![15, -16],
+            fixed32: vec![17, 18],
+            fixed64: vec![19, 20],
+            sfixed32: vec![21, -22],
+            sfixed64: vec![23, 24],
+            r#bool: vec![true, false],
+            string: vec!["25".to_owned(), "26".to_owned()],
+            bytes: vec![b"27".to_vec(), b"28".to_vec()],
+        },
+        ".test.ScalarArrays",
+    );
+}
+
 fn roundtrip<T>(message: &T, message_name: &str)
 where
     T: PartialEq + Debug + Message + Default,
