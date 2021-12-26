ffcf890a65cd47082dc257d5c2dfef13fcfcc68e
Andrew Hickman <andrew.hickman1@sky.com>
2021-12-26 14:07:22 +0000

    Move DescriptorError into module

---
+++ src/descriptor/error.rs
@@ -0,0 +1,116 @@
+use std::fmt;
+
+/// An error that may occur while creating a [`FileDescriptor`].
+#[derive(Debug)]
+pub struct DescriptorError {
+    kind: DescriptorErrorKind,
+}
+
+#[derive(Debug)]
+enum DescriptorErrorKind {
+    TypeNotFound {
+        name: String,
+    },
+    TypeAlreadyExists {
+        name: String,
+    },
+    UnknownSyntax {
+        syntax: String,
+    },
+    InvalidMapEntry {
+        name: String,
+    },
+    InvalidDefaultValue {
+        name: String,
+        field: String,
+        value: String,
+    },
+    EmptyEnum,
+}
+
+impl DescriptorError {
+    pub(super) fn type_not_found(name: impl ToString) -> Self {
+        DescriptorError {
+            kind: DescriptorErrorKind::TypeNotFound {
+                name: name.to_string(),
+            },
+        }
+    }
+
+    pub(super) fn type_already_exists(name: impl ToString) -> Self {
+        DescriptorError {
+            kind: DescriptorErrorKind::TypeAlreadyExists {
+                name: name.to_string(),
+            },
+        }
+    }
+
+    pub(super) fn unknown_syntax(syntax: impl ToString) -> Self {
+        DescriptorError {
+            kind: DescriptorErrorKind::UnknownSyntax {
+                syntax: syntax.to_string(),
+            },
+        }
+    }
+
+    pub(super) fn invalid_map_entry(name: impl ToString) -> Self {
+        DescriptorError {
+            kind: DescriptorErrorKind::InvalidMapEntry {
+                name: name.to_string(),
+            },
+        }
+    }
+
+    pub(super) fn invalid_default_value(
+        name: impl ToString,
+        field: impl ToString,
+        value: impl ToString,
+    ) -> Self {
+        DescriptorError {
+            kind: DescriptorErrorKind::InvalidDefaultValue {
+                name: name.to_string(),
+                field: field.to_string(),
+                value: value.to_string(),
+            },
+        }
+    }
+
+    pub(super) fn empty_enum() -> Self {
+        DescriptorError {
+            kind: DescriptorErrorKind::EmptyEnum,
+        }
+    }
+}
+
+impl std::error::Error for DescriptorError {}
+
+impl fmt::Display for DescriptorError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match &self.kind {
+            DescriptorErrorKind::TypeNotFound { name } => {
+                write!(f, "the message or enum type '{}' was not found", name)
+            }
+            DescriptorErrorKind::TypeAlreadyExists { name } => {
+                write!(
+                    f,
+                    "the message or enum type '{}' is defined multiple times",
+                    name
+                )
+            }
+            DescriptorErrorKind::UnknownSyntax { syntax } => {
+                write!(f, "the syntax '{}' is not recognized", syntax)
+            }
+            DescriptorErrorKind::InvalidMapEntry { name } => {
+                write!(f, "the map entry message '{}' is invalid", name)
+            }
+            DescriptorErrorKind::InvalidDefaultValue { name, field, value } => {
+                write!(
+                    f,
+                    "the default value '{}' for field '{}' of message '{}' is invalid",
+                    value, field, name
+                )
+            }
+            DescriptorErrorKind::EmptyEnum => write!(f, "enums must have at least one value"),
+        }
+    }
+}
--- src/descriptor/mod.rs
+++ src/descriptor/mod.rs
@@ -1,4 +1,7 @@
+mod error;
 mod ty;
+
+pub use self::error::DescriptorError;
 
 use std::{fmt, sync::Arc};
 
@@ -110,34 +113,6 @@ pub struct EnumDescriptor {
 pub struct EnumValueDescriptor {
     parent: EnumDescriptor,
     number: i32,
-}
-
-/// An error that may occur while creating a [`FileDescriptor`].
-#[derive(Debug)]
-pub struct DescriptorError {
-    kind: DescriptorErrorKind,
-}
-
-#[derive(Debug)]
-enum DescriptorErrorKind {
-    TypeNotFound {
-        name: String,
-    },
-    TypeAlreadyExists {
-        name: String,
-    },
-    UnknownSyntax {
-        syntax: String,
-    },
-    InvalidMapEntry {
-        name: String,
-    },
-    InvalidDefaultValue {
-        name: String,
-        field: String,
-        value: String,
-    },
-    EmptyEnum,
 }
 
 impl FileDescriptor {
@@ -466,92 +441,5 @@ impl EnumDescriptor {
 impl EnumValueDescriptor {
     pub fn number(&self) -> i32 {
         self.number
-    }
-}
-
-impl std::error::Error for DescriptorError {}
-
-impl fmt::Display for DescriptorError {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match &self.kind {
-            DescriptorErrorKind::TypeNotFound { name } => {
-                write!(f, "the message or enum type '{}' was not found", name)
-            }
-            DescriptorErrorKind::TypeAlreadyExists { name } => {
-                write!(
-                    f,
-                    "the message or enum type '{}' is defined multiple times",
-                    name
-                )
-            }
-            DescriptorErrorKind::UnknownSyntax { syntax } => {
-                write!(f, "the syntax '{}' is not recognized", syntax)
-            }
-            DescriptorErrorKind::InvalidMapEntry { name } => {
-                write!(f, "the map entry message '{}' is invalid", name)
-            }
-            DescriptorErrorKind::InvalidDefaultValue { name, field, value } => {
-                write!(
-                    f,
-                    "the default value '{}' for field '{}' of message '{}' is invalid",
-                    value, field, name
-                )
-            }
-            DescriptorErrorKind::EmptyEnum => write!(f, "enums must have at least one value"),
-        }
-    }
-}
-
-impl DescriptorError {
-    fn type_not_found(name: impl ToString) -> Self {
-        DescriptorError {
-            kind: DescriptorErrorKind::TypeNotFound {
-                name: name.to_string(),
-            },
-        }
-    }
-
-    fn type_already_exists(name: impl ToString) -> Self {
-        DescriptorError {
-            kind: DescriptorErrorKind::TypeAlreadyExists {
-                name: name.to_string(),
-            },
-        }
-    }
-
-    fn unknown_syntax(syntax: impl ToString) -> Self {
-        DescriptorError {
-            kind: DescriptorErrorKind::UnknownSyntax {
-                syntax: syntax.to_string(),
-            },
-        }
-    }
-
-    fn invalid_map_entry(name: impl ToString) -> Self {
-        DescriptorError {
-            kind: DescriptorErrorKind::InvalidMapEntry {
-                name: name.to_string(),
-            },
-        }
-    }
-
-    fn invalid_default_value(
-        name: impl ToString,
-        field: impl ToString,
-        value: impl ToString,
-    ) -> Self {
-        DescriptorError {
-            kind: DescriptorErrorKind::InvalidDefaultValue {
-                name: name.to_string(),
-                field: field.to_string(),
-                value: value.to_string(),
-            },
-        }
-    }
-
-    fn empty_enum() -> Self {
-        DescriptorError {
-            kind: DescriptorErrorKind::EmptyEnum,
-        }
     }
 }
