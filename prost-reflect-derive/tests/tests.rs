ab2d1fc14df8cd6509ae7b13bf30b1b7c0e4af9a
Andrew Hickman <andrew.hickman1@sky.com>
2021-12-31 22:37:46 +0000

    Skip compile-fail tests when running under tarpaulin

--- prost-reflect-derive/tests/tests.rs
+++ prost-reflect-derive/tests/tests.rs
@@ -5,7 +5,11 @@ fn tests() {
     tests.pass("tests/basic.rs");
     tests.pass("tests/ignore_enum.rs");
     tests.pass("tests/multiple_attr.rs");
-    tests.compile_fail("tests/attr_unknown_field.rs");
-    tests.compile_fail("tests/missing_attr.rs");
-    tests.compile_fail("tests/missing_name.rs");
+
+    // Tarpaulin runs using the nightly compiler, which can result in different diagnostics
+    if cfg!(not(tarpaulin)) {
+        tests.compile_fail("tests/attr_unknown_field.rs");
+        tests.compile_fail("tests/missing_attr.rs");
+        tests.compile_fail("tests/missing_name.rs");
+    }
 }
