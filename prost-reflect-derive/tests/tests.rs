a7246e37bf1e2a9f782cadcb2ec32b5910884f9d
Andrew Hickman <andrew.hickman1@sky.com>
2021-12-31 22:44:10 +0000

    Just use tarpaulin on stable

--- prost-reflect-derive/tests/tests.rs
+++ prost-reflect-derive/tests/tests.rs
@@ -6,10 +6,7 @@ fn tests() {
     tests.pass("tests/ignore_enum.rs");
     tests.pass("tests/multiple_attr.rs");
 
-    // Tarpaulin runs using the nightly compiler, which can result in different diagnostics
-    if cfg!(not(tarpaulin)) {
-        tests.compile_fail("tests/attr_unknown_field.rs");
-        tests.compile_fail("tests/missing_attr.rs");
-        tests.compile_fail("tests/missing_name.rs");
-    }
+    tests.compile_fail("tests/attr_unknown_field.rs");
+    tests.compile_fail("tests/missing_attr.rs");
+    tests.compile_fail("tests/missing_name.rs");
 }
