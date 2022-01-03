1e9f7b68f95294ce0f5f8d9674ad0fd40ee33064
Andrew Hickman <andrew.hickman1@sky.com>
2022-01-03 00:21:22 +0000

    Add a helper to convert a `ReflectMessage` to a `DynamicMessage`

--- prost-reflect/src/reflect.rs
+++ prost-reflect/src/reflect.rs
@@ -1,11 +1,26 @@
 use prost::Message;
 
-use crate::MessageDescriptor;
+use crate::{DynamicMessage, MessageDescriptor};
 
 /// Trait for message types that support reflection.
 pub trait ReflectMessage: Message {
     /// Gets a [`MessageDescriptor`] describing the type of this message.
     fn descriptor(&self) -> MessageDescriptor;
+
+    /// Converts this message into an instance of [`DynamicMessage`] by going
+    /// through the byte representation.
+    fn transcode_to_dynamic(&self) -> DynamicMessage
+    where
+        Self: Sized,
+    {
+        let mut message = DynamicMessage::new(self.descriptor());
+        // This can only fail if `self.descriptor` returns a descriptor incompatible with the
+        // actual serialized bytes.
+        message
+            .transcode_from(self)
+            .expect("error converting to dynamic message");
+        message
+    }
 }
 
 impl<M> ReflectMessage for Box<M>
