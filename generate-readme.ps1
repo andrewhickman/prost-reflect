3d969cc23a296dfe1a630e0e55bd9cc3694f7b72
Andrew Hickman <andrew.hickman1@sky.com>
2022-02-20 15:06:12 +0000

    Fix doc files not included in package

--- generate-readme.ps1
+++ generate-readme.ps1
@@ -3,10 +3,10 @@ $ErrorActionPreference = "Stop"
 Push-Location $PSScriptRoot
 try {
     (Get-Content "README.tpl" -Raw) `
-        -replace "{{intro}}", (Get-Content "doc/intro.md" -Raw) `
-        -replace "{{decoding}}", (Get-Content "doc/decoding.md" -Raw) `
-        -replace "{{json}}", (Get-Content "doc/json.md" -Raw) `
-        -replace "{{reflect}}", (Get-Content "doc/reflect.md" -Raw) `
+        -replace "{{intro}}", (Get-Content "prost-reflect/doc/intro.md" -Raw) `
+        -replace "{{decoding}}", (Get-Content "prost-reflect/doc/decoding.md" -Raw) `
+        -replace "{{json}}", (Get-Content "prost-reflect/doc/json.md" -Raw) `
+        -replace "{{reflect}}", (Get-Content "prost-reflect/doc/reflect.md" -Raw) `
     | Set-Content "README.md"
 }
 finally { Pop-Location }
